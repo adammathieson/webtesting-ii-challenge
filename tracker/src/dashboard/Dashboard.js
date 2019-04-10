a0f9e41b80961fc59729823120cdc3f4fde00497
Adam Mathieson <mathieson1222@gmail.com>
2019-04-09 17:33:05 -0700

    adds logic to Dashboard.js, count functions as expected

--- tracker/src/dashboard/Dashboard.js
+++ tracker/src/dashboard/Dashboard.js
@@ -1,25 +1,80 @@
-import React from 'react'
+import React from "react";
 
 class Dashboard extends React.Component {
-    state = {
-        ball: '',
-        strike: '',
-        foul: '',
-        out: ''
-    }
-    
+  state = {
+    strike: 0,
+    ball: 0,
+    foul: 0,
+    out: 0
+  };
+
   render() {
     return (
       <div>
         <h2>Dashboard</h2>
-        <button>Ball</button>
-        <button>Strike</button>
-        <button>Foul</button>
-        <button>Out</button>
-
+        <button onClick={this.strike}>Strike</button>
+        <button onClick={this.ball}>Ball</button>
+        <button onClick={this.foul}>Foul</button>
+        <button onClick={this.out}>Out</button>
       </div>
-    )
+    );
   }
+
+  strike = () => {
+    if (this.state.strike === 2 && this.state.out < 2) {
+      this.setState({
+        strike: 0,
+        ball: 0,
+        foul: 0,
+        out: this.state.out + 1
+      });
+    } else if (this.state.strike === 2 && this.state.out === 2) {
+      this.setState({
+        strike: 0,
+        ball: 0,
+        foul: 0,
+        out: 0
+      });
+    } else {
+      this.setState({ strike: this.state.strike + 1 });
+    }
+  };
+
+  ball = () => {
+    if (this.state.ball === 3) {
+      this.setState({
+        strike: 0,
+        ball: 0,
+        foul: 0
+      });
+    } else {
+      this.setState({ ball: this.state.ball + 1 });
+    }
+  };
+
+  foul = () => {
+    if (this.state.strike < 2) {
+      this.setState({
+        strike: this.state.strike + 1,
+        foul: this.state.foul + 1
+      });
+    } else {
+      this.setState({ foul: this.state.foul + 1 });
+    }
+  };
+
+  out = () => {
+    if (this.state.out === 2) {
+      this.setState({
+        strike: 0,
+        ball: 0,
+        foul: 0,
+        out: 0
+      });
+    } else {
+        this.setState({ out: this.state.out + 1 })
+    }
+  };
 }
 
 export default Dashboard;
--- tracker/src/display/Display.js
+++ tracker/src/display/Display.js
@@ -3,7 +3,23 @@ import React from 'react';
 function Display(props) {
   return (
     <div>
-      <h2>Player's At Bat</h2>
+      <h2>Pitch Count</h2>
+        <div className='strikes'>
+          <h3>Strikes</h3>
+          <p></p>
+        </div>
+        <div className='balls'>
+          <h3>Balls</h3>
+          <p></p>
+        </div>
+        <div className='fouls'>
+          <h3>Fouls</h3>
+          <p></p>
+        </div>
+        <div className='outs'>
+          <h3>Outs</h3>
+          <p></p>
+        </div>
     </div>
   )
 }
